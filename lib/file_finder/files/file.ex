4ef518d6ae1be3350c68eb47b23db04f831d732a
Stuart Dum <stuart.dum@gmail.com>
2022-06-08 06:38:50 -0400

    Build out shopify api interactions

--- lib/file_finder/files/file.ex
+++ lib/file_finder/files/file.ex
@@ -2,70 +2,6 @@ defmodule FileFinder.Files.File do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @shopify_ids_query """
-    query ShopifyFileIds($cursor: String) {
-      files(after: $cursor, first: 250, query: "status:READY") {
-        nodes {
-          ... on GenericFile {
-            id
-          }
-          ... on MediaImage {
-            id
-          }
-          ... on Video {
-            id
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-        }
-      }
-    }
-  """
-
-  @shopify_file_query """
-  query ShopifyFile($id: ID!) {
-    node(id: $id) {
-      __typename
-      ... on GenericFile {
-        alt
-        createdAt
-        preview {
-          image {
-            url
-          }
-        }
-        url
-      }
-      ... on MediaImage {
-        alt
-        createdAt
-        image {
-          url
-        }
-        preview {
-          image {
-            url
-          }
-        }
-      }
-      ... on Video {
-        alt
-        createdAt
-        originalSource {
-          url
-        }
-        preview {
-          image {
-            url
-          }
-        }
-      }
-    }
-  }
-  """
-
   schema "files" do
     field :alt, :string
     field :preview_url, :string
@@ -104,8 +40,148 @@ defmodule FileFinder.Files.File do
     |> unique_constraint(:url)
   end
 
-  def new_file_changeset_from_shopify_id(shopify_id, shop) do
-    case query_shopify_file(shopify_id, shop) do
+  @shopify_ids_query """
+    query ShopifyFileIds($cursor: String) {
+      files(after: $cursor, first: 250, query: "status:READY") {
+        nodes {
+          ... on GenericFile {
+            id
+          }
+          ... on MediaImage {
+            id
+          }
+          ... on Video {
+            id
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  """
+
+  @shopify_file_node """
+    __typename
+    ... on GenericFile {
+      alt
+      createdAt
+      preview {
+        image {
+          url
+        }
+      }
+      url
+    }
+    ... on MediaImage {
+      alt
+      createdAt
+      image {
+        url
+      }
+      preview {
+        image {
+          url
+        }
+      }
+    }
+    ... on Video {
+      alt
+      createdAt
+      originalSource {
+        url
+      }
+      preview {
+        image {
+          url
+        }
+      }
+    }
+  """
+
+  @shopify_file_query """
+    query ShopifyFile($id: ID!) {
+      node(id: $id) {
+        #{@shopify_file_node}
+      }
+    }
+  """
+
+  @shopify_stage_upload_query """
+    mutation stageUpload($input: [StagedUploadInput!]!) {
+      stagedUploadsCreate(input: $input) {
+        stagedTargets {
+          parameters {
+            name
+            value
+          }
+          resourceUrl
+          url
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  """
+
+  @shopify_file_create_query """
+    mutation fileCreate($files: [FileCreateInput!]!) {
+      fileCreate(files: $files) {
+        files {
+          #{@shopify_file_node}
+        }
+        userErrors {
+          code
+          field
+          message
+        }
+      }
+    }
+  """
+
+  @doc """
+  Returns file ids from Shopify.
+
+  ## Examples
+
+      iex> request_shopify_ids(shop)
+      {:ok, ["gid://shopify/MediaImage/123456789"]}
+  """
+  def request_shopify_ids(shop) do
+    request_shopify_ids_help(nil, shop, [])
+  end
+
+  defp request_shopify_ids_help(cursor, shop, shopify_file_ids) do
+    case send_shopify_request(@shopify_ids_query, %{cursor: cursor}, shop) do
+      {:ok, %Neuron.Response{body: %{"data" => %{"files" => files}}}} ->
+        update =
+          shopify_file_ids
+          |> Enum.concat(Enum.map(files["nodes"], fn %{"id" => id} -> id end))
+
+        if files["pageInfo"]["hasNextPage"] do
+          files["pageInfo"]["endCursor"]
+          |> request_shopify_ids_help(shop, update)
+        else
+          {:ok, update}
+        end
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  @doc """
+  Creates a changeset for a new file based on the Shopify file.
+  ## Examples
+
+      iex> request_changeset("gid://shopify/MediaImage/123456789", shop)
+      {:ok, #Ecto.Changeset{}}
+  """
+  def request_changeset(shopify_id, shop) do
+    case request_shopify_file(shopify_id, shop) do
       {:ok, node} ->
         {type, url} =
           case node["__typename"] do
@@ -135,12 +211,8 @@ defmodule FileFinder.Files.File do
     end
   end
 
-  def query_shopify_ids(shop) do
-    query_shopify_ids_help(nil, shop, [])
-  end
-
-  defp query_shopify_file(shopify_id, shop) do
-    case query_shopify_api(@shopify_file_query, %{id: shopify_id}, shop) do
+  defp request_shopify_file(shopify_id, shop) do
+    case send_shopify_request(@shopify_file_query, %{id: shopify_id}, shop) do
       {:ok, %Neuron.Response{body: %{"data" => %{"node" => node}}}} ->
         {:ok, node}
 
@@ -149,18 +221,17 @@ defmodule FileFinder.Files.File do
     end
   end
 
-  defp query_shopify_ids_help(cursor, shop, shopify_file_ids) do
-    case query_shopify_api(@shopify_ids_query, %{cursor: cursor}, shop) do
-      {:ok, %Neuron.Response{body: %{"data" => %{"files" => files}}}} ->
-        update =
-          shopify_file_ids
-          |> Enum.concat(Enum.map(files["nodes"], fn %{"id" => id} -> id end))
+  @doc """
+  """
+  def create_shopify_file(file, alt, metadata, shop) do
+    case stage_upload(metadata, shop) do
+      {:ok, stage} ->
+        case upload_to_stage(file, stage["url"], stage["parameters"]) do
+          {:ok, _} ->
+            shopify_create(stage["resourceUrl"], alt, metadata.mimetype, shop)
 
-        if files["pageInfo"]["hasNextPage"] do
-          files["pageInfo"]["endCursor"]
-          |> query_shopify_ids_help(shop, update)
-        else
-          {:ok, update}
+          {:error, error} ->
+            {:error, error}
         end
 
       {:error, error} ->
@@ -168,7 +239,96 @@ defmodule FileFinder.Files.File do
     end
   end
 
-  defp query_shopify_api(query, vars, shop) do
+  # TODO: make private
+  def stage_upload(metadata, shop) do
+    vars = %{
+      input: %{
+        fileSize: metadata.filesize,
+        filename: metadata.filename,
+        httpMethod: "POST",
+        mimeType: metadata.mimetype,
+        resource: mimetype_to_content_type(metadata.mimetype)
+      }
+    }
+
+    request = send_shopify_request(@shopify_stage_upload_query, vars, shop)
+
+    case request do
+      {:ok,
+       %Neuron.Response{
+         body: %{
+           "data" => %{
+             "stagedUploadsCreate" => %{"stagedTargets" => [staged | _]}
+           }
+         }
+       }} ->
+        {:ok, staged}
+
+      {:ok,
+       %Neuron.Response{
+         body: %{
+           "data" => %{"stagedUploadsCreate" => %{"userErrors" => errors}}
+         }
+       }} ->
+        {:error, errors}
+
+      {:ok, %Neuron.Response{body: %{"errors" => errors}}} ->
+        {:error, errors}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  # TODO: make private
+  def upload_to_stage(file, url, params) do
+    # TODO: maybe use "form-data" method
+    # https://www.shopify.in/partners/blog/upload-files-graphql-react
+    # https://github.com/edgurgel/httpoison/issues/237
+    # https://stackoverflow.com/questions/33557133/http-post-multipart-with-named-file
+    # https://elixirforum.com/t/httpoison-post-multipart-with-more-form-than-the-file/4222/5
+    # HTTPoison.post(
+    #  url,
+    #  {:multipart,
+    #   [
+    #     {:file, file, {"form-data", [{:name, ""}, {:filename, ""}]}, []}
+    #   ]}
+    # )
+    HTTPoison.post(
+      url,
+      {:multipart,
+       Enum.map(params, &{String.to_atom(&1["name"]), &1["value"]}) ++
+         {:file, file, []}}
+    )
+  end
+
+  # TODO: make private
+  def shopify_create(url, alt, mimetype, shop) do
+    vars = %{
+      files: %{
+        alt: alt,
+        contentType: mimetype_to_content_type(mimetype),
+        originalSource: url
+      }
+    }
+
+    send_shopify_request(@shopify_file_create_query, vars, shop)
+  end
+
+  defp mimetype_to_content_type(mimetype) do
+    case List.first(String.split(mimetype, "/")) do
+      "image" ->
+        "IMAGE"
+
+      "video" ->
+        "VIDEO"
+
+      _ ->
+        "FILE"
+    end
+  end
+
+  defp send_shopify_request(query, vars, shop) do
     config = Application.fetch_env!(:neuron, FileFinder.Files.File)
 
     Neuron.Config.set(url: "https://#{shop.name}" <> config[:endpoint])
