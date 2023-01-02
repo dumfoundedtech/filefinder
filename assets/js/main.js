3c09832c267813b18972b7be65d2ff81c061689d
Stuart Dum <stuart.dum@gmail.com>
2023-01-02 14:06:11 -0500

    Fine tune styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,16 @@ app.ports.copyToClipboard.subscribe((text) =>
   navigator.clipboard.writeText(text)
 );
 
-app.ports.toggleModal.subscribe(
-  () => (
-    (modal = document.getElementById("modal")),
-    modal.open ? modal.close() : modal.showModal()
-  )
-);
+app.ports.toggleModal.subscribe(() => {
+  const modal = document.getElementById("modal");
+
+  if (modal.open) {
+    document.body.className = "";
+    modal.close();
+  } else {
+    document.body.className = "noscroll";
+    modal.showModal();
+  }
+
+  modal.addEventListener("close", (_) => (document.body.className = ""));
+});
